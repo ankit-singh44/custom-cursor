45c18e3c8ea153ee4d6dfc28a38c276163775b5e
Ankit Kumar Singh <70136925+ankit-singh44@users.noreply.github.com>
2024-05-02 12:34:59 +0530

    custom sursor creation

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-var a=900;
+var main=document.querySelector("#main");
+
+var cursor=document.querySelector(".cursor");
+
+main.addEventListener("mousemove",function(dets){
+    cursor.style.left=dets.x+"px";
+    cursor.style.top=dets.y+"px";
+})
